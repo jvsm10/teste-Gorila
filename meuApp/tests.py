5a4187ad2fe8f469002d9c2837d0cff0179f6820
joao victor <jvsm10@gmail.com>
2021-10-11 15:25:59 -0300

    testes

--- meuApp/tests.py
+++ meuApp/tests.py
@@ -1,3 +1,14 @@
+from django.http import response
 from django.test import TestCase
+from .models import CDB
 
-# Create your tests here.
+# Teste Post
+class TestPost(TestCase):
+    def TestRequestMessage(self):
+        data = {
+            "investmentDate": "2016-11-14",
+            "cdbRate": 103.5,
+            "currentDate": "2016-12-26"
+        }
+        response = self.client.post("/cdb/",data=data)
+        self.assertEqual(CDB.objects.count(),1)
